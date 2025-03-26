db37e2c2cc05380e43964e8abe4ddffa60613687
harshbarger <donovan@inquisoft.com>
2025-03-26 13:52:59 -0400

    fix: change implementation of match to return correct backing class in built version

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import { MatchNoArg } from "./MatchNoArg";
 export function match<T, U>(): MatchNoArg<U>;
 export function match<T, U>(value: T): Match<T, U>;
 export function match<T, U>(value?: T): Match<T, U> | MatchNoArg<U> {
-  return value === undefined
+  return arguments.length === 0
     ? new MatchNoArg<U>()
-    : new Match<T, U>(value);
+    : new Match<T, U>(value as T);
 }
 
 export function asIs<T>(value: T): T {
